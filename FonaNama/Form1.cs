a9e2f4660fdec47a31f68b48926c4a3859a9702f
ATOM-Games <silatrotila0atom@gmail.com>
2021-03-26 10:42:02 +0300

    Method Fonemona v1.3

---
+++ FonaNama/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FonaNama
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public double sislo = 0.927465381;
+        public int count;
+        public int[] X;
+        public int[] Y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //sislo = FoneNeiMan(sislo);
+            sislo = r.NextDouble();//* 0.927465381;
+            //MessageBox.Show(sislo.ToString());
+            count = Convert.ToInt32(textBox1.Text);
+            //
+            X = new int[count]; Y = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                sislo = FoneNeiMan(sislo);
+                X[i] = (int)(sislo * count);
+            }
+            //
+            chart1.Series["Series1"].Points.DataBindXY(Y, X);
+
+        }
+        char[] newFon;
+        string nstrfor;
+        double neewFon;
+        public double FoneNeiMan(double last)
+        {
+            newFon = (last * last).ToString().ToCharArray();
+            nstrfor = "0," + ((newFon.Length >= 5) ? newFon[4] : '0') +
+                ((newFon.Length >= 6) ? newFon[5] : '0') +
+                ((newFon.Length >= 7) ? newFon[6] : '0') +
+                ((newFon.Length >= 8) ? newFon[7] : '0');
+            //neewFon = Convert.ToDouble(nstrfor);
+            //MessageBox.Show(neewFon.ToString());
+            neewFon = Convert.ToDouble(Math.Floor(Convert.ToDecimal(last) * Convert.ToDecimal(last) * 1e6M) / 1e4M % 1);
+            return neewFon;
+        }
+    }
+}
---
+++ FonaNama/Random.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FonaNama
+{
+    class Randome
+    {
+        public double[] rand = { 
+                                   0.958524931202887,
+                                   0.419451157757757,
+                                   0.064834817342889,
+                                   0.926384182612591,
+                                   0.507446259496476,
+                                   0.799762028641888
+                               };
+        int i = 0;
+        public double NextDouble() {
+            if (i >= rand.Length-1) i = 0; else i++;
+            return rand[i];
+        }
+    }
+}
